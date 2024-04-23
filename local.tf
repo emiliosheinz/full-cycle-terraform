cf16d28197d61a20a09abfecb3cafe9202fd3a11
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2024-04-23 16:12:45 -0300

    feat: create variable

--- local.tf
+++ local.tf
@@ -4,5 +4,11 @@
 # example = Rource's name
 resource "local_file" "example" {
   filename = "example.txt"
-  content = "Full Cycle"
+  content = var.file_content
+}
+
+variable "file_content" {
+  type = string
+  # default = "Hello World"
+  # description = "This is the variable description"
 }
