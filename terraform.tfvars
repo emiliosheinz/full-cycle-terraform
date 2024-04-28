0d6820aff279e48b366cd13ff006b24c7136a3f1
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2024-04-28 17:29:29 -0300

    refactor: subnets and variables

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+# This file should not be commited, but we are doing so for studying purposes
+prefix = "fullcycle"
--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,19 @@
-resource "aws_vpc" "new-vpc" {
+resource "aws_vpc" "vpc" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name = "fullcycle-vpc"
+    Name = "${var.prefix}-vpc"
+  }
+}
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "subnets" {
+  count = 2
+  vpc_id = aws_vpc.vpc.id
+  cidr_block = "10.0.${count.index}.0/24"
+  map_public_ip_on_launch = true
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  tags = {
+    Name = "${var.prefix}-subnet-${count.index}"
   }
 }
