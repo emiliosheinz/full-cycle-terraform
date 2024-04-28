977cb535cc466333c533061ea41909a31a1839e8
Emilio Schaedler Heinzmann <emiliosheinz@gmail.com>
2024-04-28 19:50:30 -0300

    feat: internet gateway and route table

--- vpc.tf
+++ vpc.tf
@@ -17,3 +17,27 @@ resource "aws_subnet" "subnets" {
     Name = "${var.prefix}-subnet-${count.index}"
   }
 }
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${var.prefix}-igw"
+  }
+}
+
+resource "aws_route_table" "rtb" {
+  vpc_id = aws_vpc.vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Name = "${var.prefix}-rtb"
+  }
+}
+
+resource "aws_route_table_association" "rtb-association" {
+  count = 2
+  route_table_id = aws_route_table.rtb.id
+  subnet_id = aws_subnet.subnets.*.id[count.index]
+}
